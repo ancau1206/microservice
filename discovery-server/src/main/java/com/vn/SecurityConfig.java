ae4ac58790f860efd04c30283fcb56d44f7f8616
ancau1206 <ancau12061997@gmail.com>
2023-07-24 11:56:52 +0700

    Revert "Revert "up code chay duoc project""
    This reverts commit 0517cdf99b34bf7438e4924e87b09c49dcf08915.

--- discovery-server/src/main/java/com/vn/SecurityConfig.java
+++ discovery-server/src/main/java/com/vn/SecurityConfig.java
@@ -1,13 +1,12 @@
-package config;
+package com.vn;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfig  {
+public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().ignoringRequestMatchers("/eureka/**");
